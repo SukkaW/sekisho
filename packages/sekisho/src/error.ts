b67a9cdebd580c91609cd5410eb4e8cf1d77e82f
SukkaW <isukkaw@gmail.com>
2026-04-28 21:22:37 +0800

    feat: special trick to bail out Next.js error report

--- packages/sekisho/src/error.ts
+++ packages/sekisho/src/error.ts
@@ -4,6 +4,24 @@ export class NotAuthenticatedError extends Error {
   public readonly name = 'NotAuthenticatedError';
   public readonly notAuthenticated = true;
   public readonly $sekishoError = true;
+  /**
+   * Normally, when React encounters an error during rendering in the server,
+   * it will find the nearest Suspense boundary and render its fallback (on
+   * the client it would find the nearest error boundary instead).
+   *
+   * For various purposes, Next.js also uses this mechanism to bail out of
+   * server-side rendering to do client-side rendering only. Thus Next.js
+   * create this magic digest 'BAILOUT_TO_CLIENT_SIDE_RENDERING' that does
+   * nothing but allow every error report (console, reportError, etc.) to
+   * skip this error
+   *
+   * Next.js doesn't handle this error in any special way. It is React itself
+   * who contains the "special handling".
+   *
+   * So we can safely re-use this digest to make sure that the error is not
+   * reported by Next.js
+   */
+  public readonly digest = 'BAILOUT_TO_CLIENT_SIDE_RENDERING';
 }
 
 export function isNeedLoginError(error: unknown): error is NotAuthenticatedError {
